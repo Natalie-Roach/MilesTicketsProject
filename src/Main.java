813b83904d151b81a97ddec0d25bbce9eb2214c7
Natalie-Roach <natalie-roach@yandex.ru>
2022-06-23 14:37:47 +0300

    Modified scr/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    public static void main (String [] args) {
-        int ticket = 6559; // Стоимость билета в рублях
-        int mile = 20; // Рублей в 1 бонусной миле
-        int bonus = ticket / mile; // Бонусных миль можно получить
-        System.out.println("Бесплатные мили за перелёты: " + bonus);
-
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 6559;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
